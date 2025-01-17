ab816541bc29cdfc6b152a3596b181dfe1a9d136
thuandev23 <thuandnk203@gmail.com>
2025-01-17 21:27:18 +0700

    init

---
+++ network/src/main/java/com/thuan/network/models/TestNetWork.kt
@@ -0,0 +1,12 @@
+package com.thuan.network.models
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun TestNetWork() {
+    Text(
+        text = "Hello ",
+    )
+}
